c948830223f143836dd990ddbdf944cc5c52fa4f
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-11-13 23:17:21 -0500

    Add `get_data_from_url()` and `download_file()`

--- src/downloader/web.rs
+++ src/downloader/web.rs
@@ -1,5 +1,22 @@
+use crate::downloader;
 use reqwest::blocking as reqwest;
+use std::io::Write;
 
-pub fn download_file(url: &str) -> Result {
-    todo!()
+pub fn get_data_from_url(url: &str) -> Result<Vec<u8>, anyhow::Error> {
+    let data = reqwest::get(url);
+    if let Ok(res) = data {
+        Ok(res.bytes()?.into())
+    } else {
+        Err(anyhow::Error::msg("Error in fetching URL."))
+    }
+}
+
+pub fn download_file(url: &str, name: &str) -> Result<(), anyhow::Error> {
+    let mut file = downloader::fs::get_or_create_file(name)?;
+    let res = file.write(&get_data_from_url(url)?);
+    if let Ok(_) = res {
+        Ok(())
+    } else {
+        Err(anyhow::Error::new(res.unwrap_err()))
+    }
 }
