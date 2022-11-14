4536c55aa2c1bc40d0158d1d658c2d356ba7f860
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-11-13 22:46:45 -0500

    Create fs sub-module

---
+++ src/downloader/fs.rs
@@ -0,0 +1,29 @@
+use std::path::PathBuf;
+use std::fs::{ self, File };
+
+pub fn get_data_dir() -> PathBuf {
+    let home_dir: PathBuf = dirs::home_dir().unwrap();
+    let data_dir: PathBuf = home_dir.join(".convi");
+    if !data_dir.exists() {
+        let _ = std::fs::create_dir(&data_dir);
+    }
+    data_dir
+}
+
+pub fn get_or_create_dir(dir_name: &str) -> PathBuf {
+    let mut data_dir = get_data_dir();
+    data_dir.push(dir_name);
+    if !data_dir.exists() {
+        let _ = fs::create_dir(&data_dir);
+    }
+    data_dir
+}
+
+pub fn get_or_create_file(file_name: &str) -> Result<File, std::io::Error> {
+    let mut data_dir = get_data_dir();
+    data_dir.push(file_name);
+    if fs::try_exists(&data_dir).is_err() {
+        let _ = fs::write(&data_dir, []);
+    }
+    File::open(&data_dir)
+}
