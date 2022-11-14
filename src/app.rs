c7b716fdda1caff696206fb8390251fb39e1f525
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-11-14 13:18:26 -0500

    Added format picker

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+use crate::FORMATS;
 use dioxus::prelude::*;
 use rfd::FileDialog;
 
@@ -7,11 +8,13 @@ pub fn App(cx: Scope) -> Element {
     let picked_path = use_state(&cx, || "".to_string());
     let mut output_pathbuf = std::path::PathBuf::new();
     let output_path = use_state(&cx, || "".to_string());
+    let format1 = use_state(&cx, || "".to_string());
+    let format2 = use_state(&cx, || "".to_string());
 
     cx.render(rsx!(
         style { [include_str!("css/global.css")] }
         h1 {
-            style: "margin-top: 4vh;",
+            // style: "margin-top: 4vh;",
             "Convi"
         }
 
@@ -39,6 +42,12 @@ pub fn App(cx: Scope) -> Element {
                 if let Some(p) = opt {
                     *picked_pb = p;
                     picked_path.set(picked_pb.to_str().unwrap().to_string());
+                    // let split: Vec<&str> = picked_path.get().split('.').collect();
+                    // dbg!(&split);
+                    // if split.len() > 1 {
+                    //     format1.set(split.last().unwrap().to_string());
+                    //     println!("format1: {}", format1.get());
+                    // }
                 }
             },
             "Set input file"
@@ -68,9 +77,52 @@ pub fn App(cx: Scope) -> Element {
                 if let Some(p) = opt {
                     *output_pb = p;
                     output_path.set(output_pb.to_str().unwrap().to_string());
+                    // let split: Vec<&str> = output_path.get().split('.').collect();
+                    // if split.len() > 1 {
+                    //     format2.set(split.last().unwrap().to_string());
+                    // }
                 }
             },
             "Set output file"
+        }
+
+        // Format selector
+        div {
+            style: "margin-top: 5vh;",
+            class: "inline-div",
+            select {
+                style: "margin-left: 0vw;",
+                class: "inline",
+                // value: "{format1}",
+                FORMATS.map(|f| {
+                    rsx!(
+                        option {
+                            "{f}"
+                        }
+                    )
+                })
+            }
+            p {
+                class: "inline",
+                "→"
+            }
+            select {
+                class: "inline",
+                FORMATS.map(|f| {
+                    rsx!(
+                        option {
+                            "{f}"
+                        }
+                    )
+                })
+            }
+        }
+
+        // Convert button
+        button {
+            class: "big-button",
+            style: "margin-top: 8vh;",
+            "Convert"
         }
     ))
 }
